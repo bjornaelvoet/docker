b14b8af925bcf7c11a77f8bef8c27319649d600e
bjornaelvoet <bjorn.aelvoet@me.com>
2020-06-17 16:09:39 +0200

    Building my own docker image for webgrabplus

--- docker-webgrabplus/entrypoint.sh
+++ docker-webgrabplus/entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-# Set up family group
-addgroup -g 1001 family 
+cd /app/wg++/bin/
 
-#adduser -D -H -G rio -s /bin/false -u 1000 rio
-
-# Set up the samba users and passwords
-while read line; do
-  username=$line
-  read line
-  password=$line
-  read line
-  userid=$line
-  #adduser -D -H -G rio -s /bin/false -u 1000 rio
-  adduser -s /sbin/nologin -h /home/samba -H -D $username -u $userid -G family
-  printf "%s\n%s\n" "$password" "$password" | smbpasswd -sa $username
-done < $SAMBA_USERS
-
-# Start samba
-smbd --foreground --no-process-group --log-stdout --configfile /conf/$SAMBA_CONF_FILE $SAMBA_OPTIONS
+# Every 8 hour means 60 * 60 * 8 = 28800 secs
+# Or just run and kill the docker process once finished
+while true
+do
+  echo "Executing..."
+  mono WebGrab+Plus.exe "/config"
+  echo "Sleeping..."  
+  sleep 28800
+done
