0cc21ebf92e3f2eb9712507eef87f3335cb9ef84
Bjorn Aelvoet <bjorn.aelvoet@me.com>
2021-01-24 19:20:47 +0100

    Upgrading webgrabplus for plex usage

---
+++ docker-webgrabplus/src/download.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+#
+wget -O WebGrabPlus_latest_install.tar.gz http://webgrabplus.com/sites/default/files/download/SW/V3.1.5/WebGrabPlus_V3.1.5_beta_install.tar.gz
+wget -O SiteIniPack_current.zip http://webgrabplus.com/sites/default/files/download/ini/SiteIniPack_current.zip
